04a85a0a27b580a80adaabe0006e16a29cab4247
Madhankumar S <madhanrock0302@gmail.com>
2026-06-11 15:59:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+/*let name="Madhan";
+var age=19;
+const height=170;
+console.log(name);
+console.log(age);
+console.log(age+5);
+console.log(age*2);
+console.log(age/2);
+console.log(age%2);
+console.log(age**2);
+age+=5;
+console.log(age);
+age-=2;
+console.log(age);
+console.log(height);
+let age1=20;
+console.log(age1);
+console.log(age1>age);
+console.log(age1<age);
+console.log(age1>=age);
+console.log(age1<=age);
+console.log(age1==age);
+console.log(age1!=age);*/
+
+let food = "Briyani";
+let Breakfast = "Dosa";
+let food2 = "idtly";
+let food3 = "Pongal";
+let food4 = "Vada";
+let food5 = "Poori";
+if (food==Breakfast) {
+   console.log("It's a nice Briyani as the morning breakfast!");
+}
+ else if (food2==Breakfast) {
+    console.log("It's a nice idtly as the morning breakfast!");
+}  
